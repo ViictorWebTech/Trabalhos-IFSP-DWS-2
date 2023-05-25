8330da0f2c0c45b7fbcf3115b8b9a6cb16f23701
ViictorWebTech <vwebtech777@gmail.com>
2023-05-24 21:44:40 -0300

    Envio da primeira atividade completa, faltam o restante das atividades

---
+++ Trabalho 2° Bim - 2° Info/Estruturas de Decisão e Repetição/Número Maior (Ordenação) - 01/header.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercício 1</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <style>
+        .titulo {
+            margin-top: .5em;
+        }
+
+        button {
+            margin-right: .25em;
+            margin-top: .5em;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="container">
+        <header>
+            <div class="titulo">
+                <h1>Exercício 1 - Número maior (ordenação)</h1>
+                <hr>
+            </div>
+        </header>
